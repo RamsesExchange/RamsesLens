7e217a09ef28f885d31b8b361a3b33396a10c4d6
69ren <111715700+69ren@users.noreply.github.com>
2023-04-28 10:35:14 +0800

    Update IGauge.sol

--- interfaces/IGauge.sol
+++ interfaces/IGauge.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity 0.8.19;
 
 interface IGauge {
-
-    function balanceOf(address) external view returns (uint256);
+   function rewardsListLength() external view returns (uint);
+   function rewards(uint) external view returns (address);
+   function balanceOf(address) external view returns (uint);
+   function derivedBalances(address) external view returns (uint);
+   function earned(address token, address account) external view returns (uint);
+   function rewardRate(address token) external view returns (uint);
 }
